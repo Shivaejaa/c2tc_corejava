a628a84d553edee8e79ba420f723794299928823
rushi techologies <rushi techologies@DESKTOP-GPRK70G.Dlink>
2023-09-29 11:13:48 +0530

    edited

--- TNS/src/Identifier.java
+++ TNS/src/Identifier.java
@@ -3,8 +3,8 @@ public class Identifier {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=10;
-		System.out.println(a);
+		int b=10;
+		System.out.println(b);
 
 	}
 
