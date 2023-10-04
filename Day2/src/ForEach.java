1e8bf9487a3919dc2730c66df96b2693ca764305
rushi techologies <rushi techologies@DESKTOP-GPRK70G.Dlink>
2023-10-04 11:12:50 +0530

    second commit

---
+++ Day2/src/ForEach.java
@@ -0,0 +1,21 @@
+
+public class ForEach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {10,20,30,40,50};
+		for(int temp:a)
+		{
+			System.out.println(temp);
+		}
+		
+		String s[]= {"hello","welcome","to","c2tc"};
+		for(String temp:s)
+		{
+			System.out.println(temp);
+		}
+
+		
+}
+}
---
+++ Day2/src/IfelseDemo.java
@@ -0,0 +1,15 @@
+
+public class IfelseDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int age=11;
+			if(age>=18) 
+				System.out.println("you are eligible to vote");
+			else
+				System.out.println("sorry you need to wait more");
+
+			System.out.println();
+	}
+
+}
